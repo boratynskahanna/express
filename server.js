59aa723de995bfe061cf60a697e7e6cc9753dd35
Hanna Boratynska <boratynskahanna@gmail.com>
2021-08-06 10:09:41 +0400

    Add handlebars, change layout

--- server.js
+++ server.js
@@ -1,41 +1,35 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
-app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/style.css'));
-  });
-  
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname, '/test.png'));
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
