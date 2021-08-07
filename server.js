8a966bc9697a121c62a9bfd0555246b81bc16653
Hanna Boratynska <boratynskahanna@gmail.com>
2021-08-07 15:12:30 +0400

    Add file upload input

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -31,6 +35,18 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
+
+app.post("/contact/send-message", upload.single("image"), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const file = req.file;
+
+  if (author && sender && title && message && file) {
+    res.render("contact", { isSent: true, fileName: file.originalname });
+  } else {
+    res.render("contact", { isError: true });
+  }
+});
+
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
